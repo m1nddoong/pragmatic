ec5bdcf07cdc44baf9c0819bfca50d440ae986ae
m1nddoong <alstjsdlr0321@korea.ac.kr>
2023-05-03 16:01:05 +0900

    django course 21 commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class HelloWorld(models.Model):
+class HelloWorld(models.Model): # DB 에 저장이 된다는 느낌. (내부 파라미터는 신경 x)
     text = models.CharField(max_length=255, null=False)
+
+
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -13,10 +16,19 @@ def hello_world(request):
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
+
         new_hello_world.text = temp
         new_hello_world.save()
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
+
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
