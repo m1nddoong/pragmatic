699d83de4b5bf6477472606f537ee673f9c34a84
m1nddoong <alstjsdlr0321@korea.ac.kr>
2023-07-03 17:47:29 +0900

    django course 42 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 # Create your views here.
@@ -30,8 +31,19 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
+        project = self.object   # object : 현재 project 페이지
+        user = self.request.user
+        # 유저가 로그인을 했는지 안했는지 확인
+        if user.is_authenticated: # 유저가 접속이 되있다면, usr 와 project 가 각각 user, project 인 구독정보를 찾고
+            subscription = Subscription.objects.filter(user=user, project=project)
+        # 유저가 접속되어있지 않는다면 구독 버튼 자체가 없기 떄문에 else 구문은 생략
+
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        # 최종적으로 템플릿으로 넘어갈때는 context_data 안에다가 구독 정보를 우리가 찾은 'subsriptiom' 으로 대체
+        # 이런식으로 구독 정보가 있는지 없는지 확인
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list,
+                                                               subscription=subscription,
+                                                               **kwargs)
 
 
 
