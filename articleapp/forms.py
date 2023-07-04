c85fef67223a20ad9a7a95f621435a5b9700f234
m1nddoong <alstjsdlr0321@korea.ac.kr>
2023-07-04 17:35:40 +0900

    django course 44 commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
+from django import forms
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    # bootstrap 에 있는 text-left 클래스 사용, 높이 자동 조절
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto; text-align: left'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all())
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
